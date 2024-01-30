aad8d4029dd427892d502cc8a96485061b2ceae1
Cullen J. O'Neill <3659487+cjon256@users.noreply.github.com>
2024-01-30 16:22:45 -0500

    Trying to get it working

--- lua/dsa.nvim/init.lua
+++ lua/dsa.nvim/init.lua
@@ -1,12 +1,9 @@
 local M
 
 M.pyleet = function(problem_id)
-    local info = vim.cmd [[
-    if executable('pyleet') == 1
-        echo system('pyleet ' .. a:problem_id)
-    else
-        echo 'pyleet is not installed'
-    ]]
+    local command = string.format(
+    "if executable('pyleet') == 1\n pyleet %s\nelse\n echo 'pyleet is not installed'\nendif\n", problem_id)
+    local info = vim.fn.system(command)
     print("Hello from pyleet")
     vim.print(info)
 end
