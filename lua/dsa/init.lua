613989b19e288427d92d6145692bda7610177446
Cullen J. O'Neill <3659487+cjon256@users.noreply.github.com>
2024-01-30 18:20:45 -0500

    Add preliminary versions of Leet commands

--- lua/dsa/init.lua
+++ lua/dsa/init.lua
@@ -1,13 +1,69 @@
 return {
     setup = function()
-        print("DSA setup")
         vim.api.nvim_create_user_command(
-            'DSAPyLeet',
+            'LeetPy',
             function(id)
-                print(string.format("updated version %s", id))
+                local handler = io.popen("pyleet info " .. id.args)
+                if handler == nil then
+                    print("handler is nil")
+                    return
+                end
+                local info = handler:read("*a")
+                if info == "" then
+                    info = "No information found"
+                else
+                    vim.print(vim.inspect(info))
+                    local dir = string.match(info, "dir: ([^\n]+)\n")
+                    local file = string.match(info, "file: (.+)\n")
+                    vim.fn.chdir(dir)
+                    vim.cmd("e " .. file)
+                end
+            end,
+            { nargs = 1 }
+        )
+
+        vim.api.nvim_create_user_command(
+            'LeetGo',
+            function(id)
+                local handler = io.popen("goleet info " .. id.args)
+                if handler == nil then
+                    print("handler is nil")
+                    return
+                end
+                local info = handler:read("*a")
+                if info == "" then
+                    info = "No information found"
+                else
+                    vim.print(vim.inspect(info))
+                    local dir = string.match(info, "dir: ([^\n]+)\n")
+                    local file = string.match(info, "file: (.+)\n")
+                    vim.fn.chdir(dir)
+                    vim.cmd("e " .. file)
+                end
+            end,
+            { nargs = 1 }
+        )
+
+        vim.api.nvim_create_user_command(
+            'LeetRust',
+            function(id)
+                local handler = io.popen("rsleet info " .. id.args)
+                if handler == nil then
+                    print("handler is nil")
+                    return
+                end
+                local info = handler:read("*a")
+                if info == "" then
+                    info = "No information found"
+                else
+                    vim.print(vim.inspect(info))
+                    local dir = string.match(info, "dir: ([^\n]+)\n")
+                    local file = string.match(info, "file: (.+)\n")
+                    vim.fn.chdir(dir)
+                    vim.cmd("e " .. file)
+                end
             end,
             { nargs = 1 }
         )
     end,
 }
-
