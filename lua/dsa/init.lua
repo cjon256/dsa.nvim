194f439e4b6b92402c554896d319fcbecc6d2587
Cullen J. O'Neill <3659487+cjon256@users.noreply.github.com>
2024-01-31 00:45:18 -0500

    Add LeetSubmit command to submit the most recent problem

--- lua/dsa/init.lua
+++ lua/dsa/init.lua
@@ -103,5 +103,39 @@ return {
             end,
             { nargs = 0 }
         )
+
+        vim.api.nvim_create_user_command(
+            'LeetSubmit',
+            function()
+                local most_recent_id = require("dsa").most_recent_id
+                if most_recent_id == 0 then
+                    print("No most recent id found")
+                    return
+                end
+                local leet_exe
+                if vim.bo.filetype == "python" then
+                    leet_exe = "pyleet"
+                elseif vim.bo.filetype == "go" then
+                    leet_exe = "goleet"
+                elseif vim.bo.filetype == "rust" then
+                    leet_exe = "rsleet"
+                else
+                    print("Unsupported filetype")
+                    return
+                end
+                local handler = io.popen(leet_exe .. " submit " .. most_recent_id)
+                if handler == nil then
+                    print("handler is nil")
+                    return
+                end
+                local info = handler:read("*a")
+                if info == "" then
+                    print("No information found")
+                else
+                    vim.print(info)
+                end
+            end,
+            { nargs = 0 }
+        )
     end,
 }
