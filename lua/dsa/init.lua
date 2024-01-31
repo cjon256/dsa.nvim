cdb4a1d51cbd625ecfaeb24ca1311e0e62bc8f60
Cullen J. O'Neill <3659487+cjon256@users.noreply.github.com>
2024-01-31 17:31:46 -0500

    Split LeetPy/Go/Rust into prompt ond non-prompt commands

--- lua/dsa/init.lua
+++ lua/dsa/init.lua
@@ -4,8 +4,12 @@ return {
     setup = function()
         vim.api.nvim_create_user_command(
             'LeetPy',
-            function()
-                local id = vim.fn.input("pyleet: enter the problem id: ")
+            function(id)
+                id = id.args
+                if id == nil then
+                    print("LeetPy requires an argument")
+                    return
+                end
                 local handler = io.popen("pyleet info " .. id)
                 if handler == nil then
                     print("handler is nil")
@@ -22,13 +26,26 @@ return {
                     vim.cmd("e " .. file)
                 end
             end,
+            { nargs = 1 }
+        )
+
+        vim.api.nvim_create_user_command(
+            'LeetPyPrompt',
+            function()
+                local id = vim.fn.input("pyleet: enter the problem id: ")
+                vim.cmd("LeetPy " .. id)
+            end,
             { nargs = 0 }
         )
 
         vim.api.nvim_create_user_command(
             'LeetGo',
-            function()
-                local id = vim.fn.input("goleet: enter the problem id: ")
+            function(id)
+                id = id.args
+                if id == nil then
+                    print("LeetGo requires an argument")
+                    return
+                end
                 local handler = io.popen("goleet info " .. id)
                 if handler == nil then
                     print("handler is nil")
@@ -38,6 +55,44 @@ return {
                 if info == "" then
                     vim.print("No information found")
                 else
+                    require("dsa").most_recent_id = id
+                    local dir = string.match(info, "dir: ([^\n]+)\n")
+                    local file = string.match(info, "file: (.+)\n")
+                    vim.fn.chdir(dir)
+                    vim.cmd("e " .. file)
+                end
+            end,
+            { nargs = 1 }
+        )
+
+        vim.api.nvim_create_user_command(
+            'LeetGoPrompt',
+            function()
+                local id = vim.fn.input("goleet: enter the problem id: ")
+                vim.cmd("LeetGo " .. id)
+            end,
+            { nargs = 0 }
+        )
+
+        vim.api.nvim_create_user_command(
+            'LeetRust',
+            function(id)
+                id = id.args
+                if id == nil then
+                    print("LeetRust requires an argument")
+                    return
+                end
+                local handler = io.popen("rsleet info " .. id)
+                if handler == nil then
+                    print("handler is nil")
+                    return
+                end
+                local info = handler:read("*a")
+                if info == "" then
+                    print("No information found")
+                else
+                    require("dsa").most_recent_id = id
+                    vim.print(vim.inspect(info))
                     local dir = string.match(info, "dir: ([^\n]+)\n")
                     local file = string.match(info, "file: (.+)\n")
                     vim.fn.chdir(dir)
@@ -48,24 +103,10 @@ return {
         )
 
         vim.api.nvim_create_user_command(
-            'LeetRust',
+            'LeetRustPrompt',
             function()
                 local id = vim.fn.input("rsleet: enter the problem id: ")
-                local handler = io.popen("rsleet info " .. id)
-                if handler == nil then
-                    print("handler is nil")
-                    return
-                end
-                local info = handler:read("*a")
-                if info == "" then
-                    print("No information found")
-                else
-                    vim.print(vim.inspect(info))
-                    local dir = string.match(info, "dir: ([^\n]+)\n")
-                    local file = string.match(info, "file: (.+)\n")
-                    vim.fn.chdir(dir)
-                    vim.cmd("e " .. file)
-                end
+                vim.cmd("LeetRust " .. id)
             end,
             { nargs = 0 }
         )
