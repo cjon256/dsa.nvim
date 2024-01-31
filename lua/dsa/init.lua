a0f3a42b5734de05ae468e58260d8f4e0e77b28b
Cullen J. O'Neill <3659487+cjon256@users.noreply.github.com>
2024-01-31 00:35:02 -0500

    revamp to use vim.fn.input for all commands, cleanup LeetTest

--- lua/dsa/init.lua
+++ lua/dsa/init.lua
@@ -4,30 +4,8 @@ return {
     setup = function()
         vim.api.nvim_create_user_command(
             'LeetPy',
-            function(id)
-                local handler = io.popen("pyleet info " .. id.args)
-                if handler == nil then
-                    print("handler is nil")
-                    return
-                end
-                local info = handler:read("*a")
-                if info == "" then
-                    vim.print("No information found")
-                else
-                    require("dsa").most_recent_id = id.args
-                    local dir = string.match(info, "dir: ([^\n]+)\n")
-                    local file = string.match(info, "file: (.+)\n")
-                    vim.fn.chdir(dir)
-                    vim.cmd("e " .. file)
-                end
-            end,
-            { nargs = 1 }
-        )
-
-        vim.api.nvim_create_user_command(
-            'LeetPyPrompt',
             function()
-                local id = vim.fn.input("Enter the problem id: ")
+                local id = vim.fn.input("pyleet: enter the problem id: ")
                 local handler = io.popen("pyleet info " .. id.args)
                 if handler == nil then
                     print("handler is nil")
@@ -49,7 +27,8 @@ return {
 
         vim.api.nvim_create_user_command(
             'LeetGo',
-            function(id)
+            function()
+                local id = vim.fn.input("goleet: enter the problem id: ")
                 local handler = io.popen("goleet info " .. id.args)
                 if handler == nil then
                     print("handler is nil")
@@ -65,12 +44,13 @@ return {
                     vim.cmd("e " .. file)
                 end
             end,
-            { nargs = 1 }
+            { nargs = 0 }
         )
 
         vim.api.nvim_create_user_command(
             'LeetRust',
-            function(id)
+            function()
+                local id = vim.fn.input("rsleet: enter the problem id: ")
                 local handler = io.popen("rsleet info " .. id.args)
                 if handler == nil then
                     print("handler is nil")
@@ -87,51 +67,38 @@ return {
                     vim.cmd("e " .. file)
                 end
             end,
-            { nargs = 1 }
+            { nargs = 0 }
         )
 
         vim.api.nvim_create_user_command(
             'LeetTest',
             function()
                 local most_recent_id = require("dsa").most_recent_id
+                if most_recent_id == 0 then
+                    print("No most recent id found")
+                    return
+                end
+                local leet_exe
                 if vim.bo.filetype == "python" then
-                    local handler = io.popen("pyleet test " .. most_recent_id)
-                    if handler == nil then
-                        print("handler is nil")
-                        return
-                    end
-                    local info = handler:read("*a")
-                    if info == "" then
-                        print("No information found")
-                    else
-                        vim.print(info)
-                    end
+                    leet_exe = "pyleet"
                 elseif vim.bo.filetype == "go" then
-                    local handler = io.popen("goleet test " .. most_recent_id)
-                    if handler == nil then
-                        print("handler is nil")
-                        return
-                    end
-                    local info = handler:read("*a")
-                    if info == "" then
-                        print("No information found")
-                    else
-                        vim.print(vim.inspect(info))
-                    end
+                    leet_exe = "goleet"
                 elseif vim.bo.filetype == "rust" then
-                    local handler = io.popen("rsleet test " .. most_recent_id)
-                    if handler == nil then
-                        print("handler is nil")
-                        return
-                    end
-                    local info = handler:read("*a")
-                    if info == "" then
-                        print("No information found")
-                    else
-                        vim.print(vim.inspect(info))
-                    end
+                    leet_exe = "rsleet"
                 else
                     print("Unsupported filetype")
+                    return
+                end
+                local handler = io.popen(leet_exe .. " test " .. most_recent_id)
+                if handler == nil then
+                    print("handler is nil")
+                    return
+                end
+                local info = handler:read("*a")
+                if info == "" then
+                    print("No information found")
+                else
+                    vim.print(info)
                 end
             end,
             { nargs = 0 }
