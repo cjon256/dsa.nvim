1214075a9750a82d725550bd7fb239a9c9025110
Cullen J. O'Neill <3659487+cjon256@users.noreply.github.com>
2024-01-30 23:28:29 -0500

    Trying to add a test command to the plugin.
    I don't know if the scoping rules will allow it...

--- lua/dsa/init.lua
+++ lua/dsa/init.lua
@@ -1,5 +1,6 @@
 -- luacheck: globals vim
 return {
+    most_recent_id = 0,
     setup = function()
         vim.api.nvim_create_user_command(
             'LeetPy',
@@ -13,6 +14,7 @@ return {
                 if info == "" then
                     vim.print("No information found")
                 else
+                    require("dsa.nvim").most_recent_id = id.args
                     local dir = string.match(info, "dir: ([^\n]+)\n")
                     local file = string.match(info, "file: (.+)\n")
                     vim.fn.chdir(dir)
@@ -63,6 +65,53 @@ return {
                 end
             end,
             { nargs = 1 }
+        )
+
+        vim.api.nvim_create_user_command(
+            'LeetTest',
+            function()
+                local most_recent_id = require("dsa.nvim").most_recent_id
+                if vim.bo.filetype == "python" then
+                    local handler = io.popen("pyleet test " .. most_recent_id)
+                    if handler == nil then
+                        print("handler is nil")
+                        return
+                    end
+                    local info = handler:read("*a")
+                    if info == "" then
+                        print("No information found")
+                    else
+                        vim.print(vim.inspect(info))
+                    end
+                elseif vim.bo.filetype == "go" then
+                    local handler = io.popen("goleet test " .. most_recent_id)
+                    if handler == nil then
+                        print("handler is nil")
+                        return
+                    end
+                    local info = handler:read("*a")
+                    if info == "" then
+                        print("No information found")
+                    else
+                        vim.print(vim.inspect(info))
+                    end
+                elseif vim.bo.filetype == "rust" then
+                    local handler = io.popen("rsleet test " .. most_recent_id)
+                    if handler == nil then
+                        print("handler is nil")
+                        return
+                    end
+                    local info = handler:read("*a")
+                    if info == "" then
+                        print("No information found")
+                    else
+                        vim.print(vim.inspect(info))
+                    end
+                else
+                    print("Unsupported filetype")
+                end
+            end,
+            { nargs = 0 }
         )
     end,
 }
